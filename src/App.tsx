de48360a5edad1708053b713febb8161dfbb51e1
waleed <waleed.sukhon77@gmail.com>
2023-03-03 19:30:35 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Header from "./Components/Header/Header";
+import "./Root.scss";
+
+function App() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,6 @@
+import "./style.scss";
+
+interface IProps {}
+export default ({}: IProps) => {
+  return <div className="Header">wad</div>;
+};
